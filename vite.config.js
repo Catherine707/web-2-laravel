f689e00a41860d0bcf395c347d04826d276ef0f0
Catherine707 <catherinecoti18@gmail.com>
2025-10-24 17:45:31 -0600

    build: especificar outDir para vite (public/build)

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    server: {
-        cors: true,
-    },
-});
+  build: {
+    outDir: 'public/build', 
+    emptyOutDir: true       
+  },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true
+    }),
+    tailwindcss()
+  ],
+  server: {
+    cors: true
+  }
+})
