3a82d09a296c7f92ebbd63d5f33d9dfb8a645685
Catherine707 <catherinecoti18@gmail.com>
2025-10-25 17:50:02 -0600

    Shim /livewire/livewire.min.js to flux; clear caches

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,10 @@ Route::view('dashboard', 'dashboard')->middleware(['auth', 'verified'])->name('d
 
 require __DIR__ . '/auth.php';
 
+Route::get('/livewire/livewire.min.js', function () {
+    $path = public_path('flux/flux.min.js');
+    abort_unless(file_exists($path), 404);
+    return response()->file($path, [
+        'Content-Type' => 'application/javascript; charset=UTF-8',
+    ]);
+});
