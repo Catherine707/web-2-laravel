3a2f1b416a26ad63a6e884a31d07ff4cd140ec37
Catherine707 <catherinecoti18@gmail.com>
2025-10-25 18:52:19 -0600

    Add login submit button

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -5,36 +5,39 @@
     <x-auth-session-status class="text-center" :status="session('status')" />
 
     <form wire:submit.prevent="login" class="flex flex-col gap-6">
-
-        <!-- Email Address -->
-<flux:input
-    wire:model="email"
-    :label="__('Email address')"
-    type="email"
-    required
-    autofocus
-    autocomplete="email"
-    placeholder="email@example.com"
-    :error="$errors->first('email')"
-/>
-
-<!-- Password -->
-<div class="relative">
     <flux:input
-        wire:model="password"
-        :label="__('Password')"
-        type="password"
+        wire:model="email"
+        :label="__('Email address')"
+        type="email"
         required
-        autocomplete="current-password"
-        :placeholder="__('Password')"
-        viewable
-        :error="$errors->first('password')"
+        autofocus
+        autocomplete="email"
+        placeholder="email@example.com"
+        :error="$errors->first('email')"
     />
 
-    @if (Route::has('password.request'))
-        <flux:link class="absolute end-0 top-0 text-sm" :href="route('password.request')" wire:navigate>
-            {{ __('Forgot your password?') }}
-        </flux:link>
-    @endif
-</div>
+    <div class="relative">
+        <flux:input
+            wire:model="password"
+            :label="__('Password')"
+            type="password"
+            required
+            autocomplete="current-password"
+            :placeholder="__('Password')"
+            viewable
+            :error="$errors->first('password')"
+        />
 
+        @if (Route::has('password.request'))
+            <flux:link class="absolute end-0 top-0 text-sm" :href="route('password.request')" wire:navigate>
+                {{ __('Forgot your password?') }}
+            </flux:link>
+        @endif
+    </div>
+
+    <div class="flex items-center justify-end">
+        <flux:button type="submit" class="w-full">
+            {{ __('Log in') }}
+        </flux:button>
+    </div>
+</form>
